d749ae40c615cecca89e8092fad757b9e49957fb
Michael Ekstrand <md@ekstrandom.net>
2024-09-06 22:02:09 -0400

    add log dirs

--- src/paths.rs
+++ src/paths.rs
@@ -1,18 +1,44 @@
 //! Utilities for locating paths.
-use std::path::PathBuf;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
+use tempdir::TempDir;
 use xdg::BaseDirectories;
 
 use crate::errors::SetupError;
 
+/// Directory for working files / pipes / streams etc.
+///
+/// Implemented as a distinct type to deal with whether or not the directory
+/// should be removed.
+#[derive(Debug)]
+pub enum WorkDir {
+    Path(PathBuf),
+    TempDir(TempDir),
+}
+
+impl AsRef<Path> for WorkDir {
+    fn as_ref(&self) -> &Path {
+        match self {
+            WorkDir::Path(pth) => pth.as_ref(),
+            WorkDir::TempDir(td) => td.as_ref(),
+        }
+    }
+}
+
 /// Get the runtime directory.
-pub fn runtime_dir() -> Result<PathBuf, SetupError> {
+pub fn runtime_dir() -> Result<WorkDir, SetupError> {
     let xdg = BaseDirectories::with_prefix("logsink")?;
     if let Ok(path) = xdg.get_runtime_directory() {
         let mut path = path.clone();
         path.push("logsink");
-        Ok(path)
+        if !fs::exists(&path)? {
+            fs::create_dir(&path)?;
+        }
+        Ok(WorkDir::Path(path))
     } else {
-        todo!()
+        Ok(WorkDir::TempDir(TempDir::new("logsink")?))
     }
 }
